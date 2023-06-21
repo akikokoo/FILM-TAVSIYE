c0d9a115f6756f2ebce959b129f08ce25433cbdd
Akif Emre Reis <akifemre611@hotmail.com>
2023-06-21 15:28:29 +0300

    optimize edilmemis hali

--- film_list/butun_liste.py
+++ film_list/butun_liste.py
@@ -2,8 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
 
+
 categoryList = ["action","adventure","animation","biography","comedy","crime","drama","family","fantasy","film_noir","history","horror","musical","music","mystery","romance","sci_fi","sport","thriller","war","western"]
 gnrList = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21"]
+
 
 def save_IMDB_xl():
 
@@ -12,7 +14,7 @@ def save_IMDB_xl():
     ws = wb.active
 
     # Write headers to the worksheet
-    ws.append(["Rank", "Name", "Year", "Rating","Category"])
+    ws.append(["Rank", "Name", "Year", "Rating", "Description", "ID"])
 
     for category, gnr in zip(categoryList,gnrList):
         url = f"https://www.imdb.com/search/title/?genres={category}&sort=user_rating,desc&title_type=feature&num_votes=25000,&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=94365f40-17a1-4450-9ea8-01159990ef7f&pf_rd_r=7ZHT2F4Q0BQ2W9670M7G&pf_rd_s=right-6&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_gnr_{gnr}"
@@ -37,13 +39,17 @@ def save_IMDB_xl():
             # Extract year
             year = movie.find("span", class_="lister-item-year").text.strip("()").replace("I) (","").replace("I","")
             
-            # Extract IMDb rating
+            # Extract IMDB rating
             rating = movie.find("div", class_="ratings-bar").strong.text
+
+            # Extract description
+            desc = movie.find_all("p", class_="text-muted")[1].text
             
+            id = movie.find("h3",class_="lister-item-header").a["href"].replace("/title/tt","").replace("/","")
+
             # Write the data to the worksheet
-            ws.append([rank, name, year, rating, category])
+            ws.append([rank, name, year, rating, desc, id])
 
     # Save the workbook
-    wb.save("butun_liste.xlsx")        
-    
+    wb.save("film_list/butun_liste.xlsx")
 save_IMDB_xl()
---
+++ film_list/main.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pandas as pd
+import random
+from butun_liste import save_IMDB_xl
+import os
+from imdb import Cinemagoer
+import random
+
+categoryList = ["action","adventure","animation","biography","comedy","crime","drama","family","fantasy","film_noir","history","horror","musical","music","mystery","romance","sci_fi","sport","thriller","war","western"]
+global data_list
+
+@st.cache_data
+def call_save_IMDB_xl():
+    return save_IMDB_xl()
+
+@st.cache_data
+def set_random_film_data_list(excel_file,selected_category):
+    data_list = []
+
+    df = pd.read_excel(excel_file)
+    film_rows = df[df['Category'] == selected_category]
+
+    names = film_rows['Name'].tolist()
+    years = film_rows['Year'].tolist()
+    ratings = film_rows['Rating'].tolist()
+    descs = film_rows["Description"].toList()
+    id = film_rows["ID"].toList()
+    
+    if selected_category == "film_noir":
+        film_number = random.randint(0,30)
+    else:
+        film_number = random.randint(0,50)
+    
+    data_list.append(names[film_number],years[film_number],ratings[film_number],id[film_number],descs[film_number])
+    
+def get_image_url(film_ID):
+    ia = Cinemagoer()
+    movie = ia.get_movie(film_ID)
+
+    url = movie['cover url']
+    url = url[:url.rindex('@') + 1] + url[url.rindex('.'):]
+
+    base, ext = os.path.splitext(url)
+    i = url.count('@')
+    s2 = url.split('@')[0]
+    url = s2 + '@' * i + ext
+    return url
+
+def main():
+    #retrieving
+    call_save_IMDB_xl()
+
+    with st.sidebar():
+        st.header("Film Öneri Uygulaması")
+        selected_category = st.selectbox("Kategori Seçiniz:", [category for category in categoryList])
+        set_random_film_data_list(r"film_list\butun_liste.xlsx",selected_category)
+        st.markdown('''
+            <br />
+            <button target="suggest">Film Öner</button>
+        ''')
+    st.markdown(f'''
+        <img src=[{get_image_url(data_list[3])} />          
+    ''')
+    
+
+
+
+if __name__ == "__main__":
+    main()
